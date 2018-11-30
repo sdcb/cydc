f724e40428872204f50fc107618861239a82ed19
sdcb <sdflysha@qq.com>
2018-11-30 09:59:10 +0800

    refine code

--- cydc/ClientApp/src/app/shared/nav-menu/nav-menu.component.ts
+++ cydc/ClientApp/src/app/shared/nav-menu/nav-menu.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-nav-menu',
