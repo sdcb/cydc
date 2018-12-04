4c799b93bb5347f30437c892bf9507d8dbc1fc45
sdcb <sdflysha@qq.com>
2018-12-04 18:01:28 +0800

    try implement api users.

--- cydc/ClientApp/src/app/admin/admin-api.service.ts
+++ cydc/ClientApp/src/app/admin/admin-api.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DataSource } from '@angular/cdk/table';
+import { CollectionViewer } from '@angular/cdk/collections';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +11,14 @@ import { HttpClient } from '@angular/common/http';
 export class AdminApiService {
   constructor(private http: HttpClient) { }
 
-  getUsers(name: string, operator: BalanceOperator, skip: number, take: number) {
-    return this.http.get<AdminUserDto[]>(`/api/admin/users?skip=${skip}&take=${take}&operator=${operator}&name=${encodeURIComponent(name)}`);
+  getUsers(query: AdminUserQuery, skip: number, take: number) {
+    return this.http.get<AdminUserDto[]>(`/api/admin/users?skip=${skip}&take=${take}&operator=${query.operator}&name=${encodeURIComponent(query.name)}`);
   }
+}
+
+export class AdminUserQuery {
+  name!: string;
+  operator!: BalanceOperator;
 }
 
 export type AdminUserDto = {
@@ -24,4 +33,28 @@ export enum BalanceOperator {
   LessThanZero = 1,
   EqualToZero = 2,
   GreaterThanZero = 3, 
+}
+
+export class ApiDataSource<T, S> extends DataSource<T> {
+  dataSubject = new BehaviorSubject<T[]>([]);
+  loading = false;
+
+  constructor(private searchApi: (searchDto: S, skip: number, take: number) => Observable<T[]>) {
+    super();
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<T[]> {
+    return this.dataSubject.asObservable();
+  }
+
+  disconnect(collectionViewer: CollectionViewer) {
+    this.dataSubject.complete();
+  }
+
+  loadData(searchDto: S, page = 1, pageSize = 12) {
+    this.loading = true;
+    this.searchApi(searchDto, (page - 1) * pageSize, pageSize)
+      .pipe(finalize(() => this.loading = false))
+      .subscribe(data => this.dataSubject.next(data));
+  }
 }
--- cydc/ClientApp/src/app/admin/users/users.component.ts
+++ cydc/ClientApp/src/app/admin/users/users.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { AdminUserDto, AdminApiService, ApiDataSource, AdminUserQuery } from '../admin-api.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,29 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  constructor(private userService: UserService) { }
+  displayedColumns = ["name", "email", "balance"];
+  query = new AdminUserQuery();
+  dataSource: ApiDataSource<AdminUserDto, AdminUserQuery>;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  constructor(
+    private userService: UserService,
+    private api: AdminApiService) {
+    this.dataSource = new ApiDataSource<AdminUserDto, AdminUserQuery>((p, s, t) => this.api.getUsers(p, s, t));
+  }
 
   async ngOnInit() {
     await this.userService.ensureAdmin();
+
+    this.dataSource.loadData(this.query, this.paginator.pageIndex, this.paginator.pageSize);
+    this.paginator.page.subscribe(() => {
+      this.dataSource.loadData(this.query, this.paginator.pageIndex, this.paginator.pageSize);
+    });
+    //this.dataSource.
   }
 }
