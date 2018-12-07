783658f2a1789485cdcd62c31bb22eb2c42beb3e
sdcb <sdflysha@qq.com>
2018-12-07 10:36:48 +0800

    hide menu when not priviledge.

--- cydc/ClientApp/src/app/shared/nav-menu/nav-menu.component.ts
+++ cydc/ClientApp/src/app/shared/nav-menu/nav-menu.component.ts
@@ -14,4 +14,12 @@ export class NavMenuComponent {
   async logout() {
     await this.userService.logout();
   }
+
+  get isAdmin() {
+    return this.userService.userStatus.isAdmin;
+  }
+
+  get isLoggedIn() {
+    return this.userService.userStatus.isLoggedIn;
+  }
 }
