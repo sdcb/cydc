e0b305ec51bde5e57dc250db095e79f515722cd1
sdcb <sdflysha@qq.com>
2018-12-28 22:31:23 +0800

    add version to home.

--- cydc/ClientApp/src/app/home/home.component.ts
+++ cydc/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { version } from "../../environments/version";
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,9 @@ export class HomeComponent {
   source = "https://github.com/sdcb/cydc";
 
   constructor(public user: UserService) {
+  }
+
+  version() {
+    return version.job + version.buildId;
   }
 }
