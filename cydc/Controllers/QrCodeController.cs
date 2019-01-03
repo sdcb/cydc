b1694627f2921288fb4a27a4c6b34e7ea1df2ac1
sdcb <sdflysha@qq.com>
2019-01-03 17:53:12 +0800

    Add QRCodeController.

---
+++ cydc/Controllers/QrCodeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using cydc.Controllers.AdmimDtos;
+using cydc.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+namespace cydc.Controllers
+{
+    [Authorize]
+    public class QrCodeController : Controller
+    {
+        public IActionResult Pay()
+        {
+            string id = "HTTPS://QR.ALIPAY.COM/LPX06836V926QKAB6FAR64";
+            byte[] qrImageData = GetQRImage(id);
+            return File(qrImageData, "image/png");
+        }
+
+        public IActionResult RedPacket()
+        {
+            string id = "lX6zc1x09185qbi6ezowgav9l67urb";
+            byte[] qrImageData = GetQRImage(id);
+            return File(qrImageData, "image/png");
+        }
+
+        private byte[] GetQRImage(string content)
+        {
+            using (var qrCodeGenerator = new QRCodeGenerator())
+            using (QRCodeData data = qrCodeGenerator.CreateQrCode(content, QRCodeGenerator.ECCLevel.L))
+            using (var qrCode = new PngByteQRCode(data))
+            {
+                return qrCode.GetGraphic(4);
+            }
+        }
+    }
+}
