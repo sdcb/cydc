530dcdd33c78fe3f049b6131dbacb929c05ffd35
sdcb <sdflysha@qq.com>
2019-01-09 08:09:35 +0800

    prompt dialog initial commit

---
+++ cydc/ClientApp/src/app/shared/dialogs/prompt/prompt.dialog.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-prompt-dialog',
+    templateUrl: 'prompt.dialog.html'
+})
+
+export class PromptDialog implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
