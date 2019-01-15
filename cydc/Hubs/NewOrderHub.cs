9bbbc5466692d5c10d60d3603eb04d93f43548d0
sdcb <sdflysha@qq.com>
2019-01-15 17:33:58 +0800

    add NewOrderHub for signalr

---
+++ cydc/Hubs/NewOrderHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace cydc.Hubs
+{
+    public class NewOrderHub : Hub<NewOrderHubClient>
+    {
+    }
+
+    public static class NewOrderHubExtensions
+    {
+        public static async Task OnNewOrder(this IHubContext<NewOrderHub, NewOrderHubClient> hubContext, int orderId)
+        {
+            await hubContext.Clients.All.OnNewOrder(orderId);
+        }
+    }
+
+    public interface NewOrderHubClient
+    {
+        Task OnNewOrder(int orderId);
+    }
+}
