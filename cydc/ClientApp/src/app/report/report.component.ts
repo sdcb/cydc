3a06dbf057a8846b8981ab741e2a52dea3a5c1eb
Flysha <sdflysha@qq.com>
2019-02-18 11:36:29 +0800

    report - show percent for normal user.

--- cydc/ClientApp/src/app/report/report.component.ts
+++ cydc/ClientApp/src/app/report/report.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ReportApi } from './report.api';
+import { UserService } from '../services/user.service';
 
 
 @Component({
@@ -8,33 +9,36 @@ import { ReportApi } from './report.api';
 })
 export class ReportComponent implements OnInit {
   days = 60;
-  dayOrdersData = empty();
-  hourOrdersData = empty();
+  dayOrdersData = this.empty();
+  hourOrdersData = this.empty();
   tasteOrders = new DataLabel();
   locationOrders = new DataLabel();
 
-  constructor(private api: ReportApi) {
+  constructor(
+    private api: ReportApi,
+    private userService: UserService) {
   }
 
   async ngOnInit() {
-    this.dayOrdersData = datasetFromArray(await this.api.dayOrders(this.days).toPromise());
-    this.hourOrdersData = datasetFromArray(await this.api.hourOrders(this.days).toPromise());
+    this.dayOrdersData = this.datasetFromArray(await this.api.dayOrders(this.days).toPromise());
+    this.hourOrdersData = this.datasetFromArray(await this.api.hourOrders(this.days).toPromise());
     this.tasteOrders = DataLabel.from(await this.api.tasteOrders(this.days).toPromise());
     this.locationOrders = DataLabel.from(await this.api.locationOrders(this.days).toPromise());
   }
+
+  datasetFromArray(data: number[], label: string | undefined = undefined) {
+    if (label === undefined) {
+      label = this.userService.userStatus.isAdmin ? "数量" : "百分比"
+    }
+    return [{ data: data, label: label }];
+  }
+
+  empty() {
+    return this.datasetFromArray([], "");
+  }
 }
 
-export function datasetFromArray(data: number[], label = "数量") {
-  return [{ data: data, label: label }];
-}
 
-export function datasetFromObject() {
-  
-}
-
-export function empty() {
-  return datasetFromArray([], "");
-}
 
 export class DataLabel {
   data = [0];
@@ -42,7 +46,7 @@ export class DataLabel {
 
   static from(obj: { [key: string]: number }): DataLabel {
     return {
-      data: Object.values(obj), 
+      data: Object.values(obj),
       labels: Object.keys(obj)
     }
   }
